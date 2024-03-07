39898e8234b03acb6999cb47bed04c002e7c6973
Valentin Palkovic <valentin@chromatic.com>
2024-03-07 14:47:22 +0100

    Remove git dirty guard

--- src/utils/git.utils.ts
+++ src/utils/git.utils.ts
@@ -2,12 +2,3 @@ import { promisify } from 'util';
 import { execFile } from 'child_process';
 
 const exec = promisify(execFile);
-
-/**
- *
- * @returns true if git is clean, false otherwise
- */
-export const isGitClean = async () => {
-    const { stdout } = await exec('git', ['status', '--porcelain']);
-    return stdout === '';
-};
